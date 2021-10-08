194d42695cd4cae44c60f5947aea6024ba77448f
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2021-10-08 15:12:27 +0530

    PY File Updated

--- hostel/hostel/doctype/deallotment_process/deallotment_process.py
+++ hostel/hostel/doctype/deallotment_process/deallotment_process.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2021, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class DeallotmentProcess(Document):
+	#frappe.db.sql(""" UPDATE `tabEmployee Hostel Allotment`  SET  start_date= '2000-09-01' where name='RAN-2021-00001';""")	
 	pass
--- hostel/hostel/doctype/room_allotment/room_allotment.py
+++ hostel/hostel/doctype/room_allotment/room_allotment.py
@@ -1,8 +1,84 @@
 # Copyright (c) 2021, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.exceptions import Redirect
 from frappe.model.document import Document
+import pandas as pd
+import datetime
+
 
 class RoomAllotment(Document):
-	pass
+	@frappe.whitelist()
+	def validate(doc):
+		Stu_info=frappe.db.sql(""" select * from `tabRoom Allotment` as RA where RA.student='%s'"""%(doc.student))
+		df1=pd.DataFrame({
+			'Al_no':[],'creation':[],'modified':[],'modified_by':[],
+			'owner':[],'docstatus':[],'parent':[],'parentfield':[],
+			'parenttype':[],'idx':[],'naming_series':[],'student':[],
+			'student_name':[],'hostel_id':[],'start_date':[],'allotment_type':[],
+			'end_date':[],'room_id':[],'room_type':[],'employee':[],'employee_name':[]
+		})
+		for t in range(len(Stu_info)):
+			s=pd.Series([Stu_info[t][0],Stu_info[t][1],Stu_info[t][2],Stu_info[t][3],Stu_info[t][4],Stu_info[t][5],
+						Stu_info[t][6],Stu_info[t][7],Stu_info[t][8],Stu_info[t][9],Stu_info[t][10],Stu_info[t][11],
+						Stu_info[t][12],Stu_info[t][13],Stu_info[t][14],Stu_info[t][15],Stu_info[t][16],Stu_info[t][17],
+						Stu_info[t][18],Stu_info[t][19],Stu_info[t][20]],
+								index=['Al_no','creation','modified','modified_by',
+										'owner','docstatus','parent','parentfield',
+										'parenttype','idx','naming_series','student',
+										'student_name','hostel_id','start_date','allotment_type',
+										'end_date','room_id','room_type','employee','employee_name'])
+			df1=df1.append(s,ignore_index=True)					
+		if len(df1)==0:
+			pass
+		else:
+			ck_data=df1[(df1['start_date']<=datetime.date.today())&(df1['end_date']>=datetime.date.today())].reset_index()
+			if len(ck_data)!=0:
+				frappe.throw("Student is already allotted in Hostel %s Room no %s"%(ck_data['hostel_id'][0],ck_data['room_id'][0]))
+			else:
+				pass
+	@frappe.whitelist()
+	def onload(doc):
+	 		# Room_info=frappe.db.sql(""" select * from `tabRoom Masters` as RM where RM.hostel_id='%s';"""%(doc.student))
+			# a=frappe.utils.get_email(frappe.session.user);
+			# print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa %s"%a)
+			if frappe.session.user!="Administrator":
+				Emp_all_info=frappe.db.sql(""" select * from `tabEmployee Hostel Allotment` where employees= '%s';"""%(frappe.session.user))
+				df2=pd.DataFrame({
+									'name':[],'employee':[],'employees':[],'start_date':[],'end_date':[],'hostel_masters':[],'user_name':[]
+								})
+				for t in range(len(Emp_all_info)):
+					s=pd.Series([Emp_all_info[t][0],Emp_all_info[t][11],Emp_all_info[t][18],Emp_all_info[t][19],Emp_all_info[t][20],Emp_all_info[t][21],
+								Emp_all_info[t][23]],
+										index=['name','employee','employees','start_date','end_date','hostel_masters','user_name'])
+					df2=df2.append(s,ignore_index=True)	
+
+				emp_hostel=[]
+				df2=df2[(df2['start_date']<=datetime.date.today())&(df2['end_date']>=datetime.date.today())].reset_index()
+				for t in range(len(df2)):
+					emp_hostel.append(df2['hostel_masters'][t])
+
+				##### Emp hostel Allotment#####	
+				into=''
+				if len(emp_hostel)==1:
+					info="(HM.hostel_name='%s') and (HM.start_date<=now() and HM.end_date>= now())"%(emp_hostel[0])
+				elif len(emp_hostel)>=1:
+					info='(HM.hostel_name IN %s ) and (HM.start_date<=now() and HM.end_date>= now())'%(str(tuple(emp_hostel)))
+				else:
+					frappe.throw("Please contact to the Aminastrator")
+
+				Hostel_sel_emp=	frappe.db.sql("""select * from `tabHostel Masters` as HM where """+info)
+				df3=pd.DataFrame({ 'Hostel_name':[]})
+				for t in range(len(Hostel_sel_emp)):
+					s=pd.Series(
+						[str(Hostel_sel_emp[t][0])],index=['Hostel_name']
+					)
+					df3=df3.append(s,ignore_index=True)	
+				print(df3['Hostel_name'])	
+			else:
+				return
+			
+	
+
+
