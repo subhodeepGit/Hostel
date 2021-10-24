d39b9fcc0f0a3dc1d5eb638414f177e97515dd94
deysubhodeep <subhodeep.dey@soulunileaders.com>
2021-10-24 22:10:25 +0530

    Some files are modified

--- hostel/hostel/doctype/indisciplinary_complaint_registration/indisciplinary_complaint_registration.py
+++ hostel/hostel/doctype/indisciplinary_complaint_registration/indisciplinary_complaint_registration.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2021, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class IndisciplinaryComplaintRegistration(Document):
 	pass
+
+
+# @frappe.whitelist()
+# @frappe.validate_and_sanitize_search_inputs
+# def ra_query(doctype, txt, searchfield, start, page_len, filters):
+#     return frappe.db.sql("""
+#         SELECT `name`,`student`,`student_name` FROM `tabRoom Allotment` WHERE `start_date` <= now() AND `end_date` >= now() 
+#     """
+#     )   
