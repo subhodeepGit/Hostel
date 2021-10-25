c04cd2c7e6714c7c1bcca48420e24b6943b3b8d6
deysubhodeep <subhodeep.dey@soulunileaders.com>
2021-10-25 20:46:25 +0530

    Some files are modified

--- hostel/hostel/doctype/room_allotment/room_allotment.js
+++ hostel/hostel/doctype/room_allotment/room_allotment.js
@@ -1,13 +1,34 @@
 // Copyright (c) 2021, SOUL and contributors
 // For license information, please see license.txt
 
+// frappe.ui.form.on('Room Allotment', {
+// 	setup: function (frm) {
+// 		frm.set_query("room_id", function () {
+// 			return {
+// 				filters: [
+// 					["Room Masters", "hostel_id", "=", frm.doc.hostel_id]
+// 				]
+// 			}
+// 		});
+// 		frm.set_query("hostel_id", function() {
+// 			return {
+// 				query: "hostel.hostel.doctype.room_allotment.room_allotment.test_query"
+// 			};
+// 		});
+// 	}
+// })
+
+
+
+
 frappe.ui.form.on('Room Allotment', {
 	setup: function (frm) {
 		frm.set_query("room_id", function () {
 			return {
-				filters: [
-					["Room Masters", "hostel_id", "=", frm.doc.hostel_id]
-				]
+				query: "hostel.hostel.doctype.room_allotment.room_allotment.vacancy_query",
+				filters: frm.doc.hostel_id == "Room Masters"  ?
+					// ["Room Masters", "hostel_id", "=", frm.doc.hostel_id]
+					{"hostel_id": frm.doc.hostel_id} : {"hostel_id": frm.doc.hostel_id}
 			}
 		});
 		frm.set_query("hostel_id", function() {
