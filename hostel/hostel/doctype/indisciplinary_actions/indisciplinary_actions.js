ecb11c6666c0553a3039880e0a2601e1f3ee018f
deysubhodeep <subhodeep.dey@soulunileaders.com>
2021-10-29 15:20:42 +0530

    dc_members doctype added

--- hostel/hostel/doctype/indisciplinary_actions/indisciplinary_actions.js
+++ hostel/hostel/doctype/indisciplinary_actions/indisciplinary_actions.js
@@ -18,3 +18,29 @@ frappe.ui.form.on("Indisciplinary Actions", "indisciplinary_complaint_registrati
 		});
 	});
 });
+frappe.ui.form.on('Indisciplinary Actions', {
+	setup: function (frm) {
+		frm.set_query("indisciplinary_complaint_registration_id", function() {
+			return {
+				query: "hostel.hostel.doctype.indisciplinary_actions.indisciplinary_actions.status_query"
+			};
+		});
+	}
+})
+// Checking duplicate 
+frappe.ui.form.on("DC Members", "emp_id", function(frm, cdt, cdn){
+    var emp_id = frm.doc.dc_member;
+    var arr =[];
+    for(var i in emp_id){
+        arr.push(emp_id[i].emp_id);
+        
+    }
+    for (var j=0;j<arr.length-1;j++){
+        for(var k=j+1;k<arr.length;k++){
+            if(arr[j] == arr[k]){                          
+                frappe.msgprint("Duplicate entry "+arr[k])
+            }
+        }
+    }
+})
+
