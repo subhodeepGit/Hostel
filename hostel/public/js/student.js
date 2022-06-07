3483048e889c7ddf423da7701eeef6d698db965b
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-06-07 13:03:04 +0530

    Pop-up msg Room Allotment Data in Student doctype

--- hostel/public/js/student.js
+++ hostel/public/js/student.js
@@ -1,25 +1,32 @@
+// Pop-up message Room Allotment Data in Student doctype in js
+
 frappe.ui.form.on("Student", "onload", function(frm) 
 { 
-	// if(frm.doc.room_allotment!=undefined)
-	// {
-        alert(frm.doc.name)
         frappe.call({
             "method": "hostel.room_allotment.get",
             args: {
-                // doctype: "Room Allotment",
                 name: frm.doc.name
             },
+       
             callback: function (data) {
-            frm.add_custom_button(__('Hostel Details'), function(){
-                alert(JSON.stringify(data))
-                // var msg = 'Student Name : ' + data.message.student_name+'<br/>';
-				var msg ='Hostel : ' + data.message.hostel_id+'<br/>';
-				msg = msg + 'Room ID : ' + data.message.room_id+'<br/>';
-				msg = msg + 'Room Number : ' + data.message.room_number + '<br/>';
-                msg = msg + 'Room Type : ' + data.message.room_type + '<br/>';
-				msgprint(__(msg));
-				});
+                frm.add_custom_button(__('Hostel Details'), function(){
+                if(Object.keys(data.message).length!=0){
+                 var msg ='Enrollment No. : ' + data.message.name+'<br/>';
+                 msg = msg + 'Hostel Registration No. : ' + data.message.hostel_registration_no+'<br/>';
+                 msg = msg + 'Hostel : ' + data.message.hostel_id+'<br/>';
+                 msg = msg + 'Room Number : ' + data.message.room_number+'<br/>';
+                 msg = msg + 'Room Type : ' + data.message.room_type+'<br/>';
+                 msg = msg + 'Start Date : ' + data.message.start_date+'<br/>';
+                 msg = msg + 'End date : ' + data.message.end_date + '<br/>';
+                 msgprint(__(msg));
+               }
+            else
+                {
+                 var daysc = "Student is a day Scholar"
+                 msgprint(__(daysc));
+                }
+            });
+    
             }
         })
-    //  }
 });
